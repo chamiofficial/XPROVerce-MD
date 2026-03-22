0f0054479466b4ef36eb8af20b24bb35c8b72067
I-AM-CHAMI <88298450+chamiofficial@users.noreply.github.com>
2026-03-22 08:58:33 +0530

    Update session.js

--- session.js
+++ session.js
@@ -37,6 +37,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session",
+SESSION_ID: process.env.SESSION_ID || "XPRO-MD~1773913806034_r1tjumn6y",
 PORT: process.env.PORT || "8000"
 };
